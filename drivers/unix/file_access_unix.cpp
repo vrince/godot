4c3a66e8062522f9fcb0bda5527007139173d8ea
Ezekiel Bethel <zek@9net.org>
2019-11-21 22:58:44 +0000

    Brings the Switch platform up to 3.2 compat.

--- drivers/unix/file_access_unix.cpp
+++ drivers/unix/file_access_unix.cpp
@@ -130,6 +130,7 @@ Error FileAccessUnix::_open(const String &p_path, int p_mode_flags) {
 		return last_error;
 	}
 
+#ifndef HORIZON_ENABLED
 	// Set close on exec to avoid leaking it to subprocesses.
 	int fd = fileno(f);
 
@@ -142,7 +143,7 @@ Error FileAccessUnix::_open(const String &p_path, int p_mode_flags) {
 		fcntl(fd, F_SETFD, opts | FD_CLOEXEC);
 #endif
 	}
-
+#endif
 	last_error = OK;
 	flags = p_mode_flags;
 	return OK;
