2d0b35c0b011f5eb704d0ddf6bfa202aaa2243f1
Ezekiel Bethel <zek@9net.org>
2020-04-06 22:25:15 +0100

    redo export a bit

--- platform/switch/export/export.cpp
+++ platform/switch/export/export.cpp
@@ -34,7 +34,6 @@
 #include "editor/editor_export.h"
 #include "editor/editor_node.h"
 #include "platform/switch/logo.gen.h"
-#include "platform/switch/icon_default.h"
 #include "scene/resources/texture.h"
 #include "core/io/packet_peer_udp.h"
 
@@ -208,7 +207,7 @@ public:
 
 		EditorProgress ep("run", "Running", 2);
 
-		// Export_temp APK.
+		print_line("Exporting...");
 		if (ep.step("Exporting...", 0)) {
 			return ERR_SKIP;
 		}
@@ -221,7 +220,7 @@ public:
 			return err;
 		}
 
-		print_line("Sending..");
+		print_line("Sending...");
 		if (ep.step("Sending...", 1)) {
 			DirAccess::remove_file_or_error(tmp_export_path);
 			return err;
@@ -247,7 +246,6 @@ public:
 			inner_args.push_back("--main-pack");
 			inner_args.push_back("sdmc:/switch/TempExport.pck");
 
-			// I don't know why no platforms actually use this method??
 			gen_export_flags(inner_args, p_debug_flags);
 			args.push_back(String(" ").join(inner_args));
 
@@ -279,7 +277,7 @@ public:
 
 	virtual List<String> get_binary_extensions(const Ref<EditorExportPreset> &p_preset) const {
 		List<String> list;
-		list.push_back("nro");
+		list.push_back("pck");
 		return list;
 	}
 
@@ -287,18 +285,15 @@ public:
 	}
 
 	virtual Error export_project(const Ref<EditorExportPreset> &p_preset, bool p_debug, const String &p_path, int p_flags = 0) {
-
 		if (!DirAccess::exists(p_path.get_base_dir())) {
 			return ERR_FILE_BAD_PATH;
 		}
-
 		String template_path = find_export_template(TEMPLATE_RELEASE);
 
 		if (template_path != String() && !FileAccess::exists(template_path)) {
 			EditorNode::get_singleton()->show_warning(TTR("Template file not found:") + "\n" + template_path);
 			return ERR_FILE_NOT_FOUND;
 		}
-
 		DirAccess *da = DirAccess::create(DirAccess::ACCESS_FILESYSTEM);
 		Error err = da->copy(template_path, p_path, 0755);
 		if (err == OK) {
@@ -307,162 +302,46 @@ public:
 			String author = p_preset->get("application/author");
 			String version = p_preset->get("application/version");
 			String icon = p_preset->get("application/icon_256x256");
+			if(icon != "" && DirAccess::exists(icon))
+			{
+				err = da->copy(icon, p_path.get_basename() + ".jpg", 0755);
+				if(err != OK) {
+					return ERR_BUG;
+				}
+			}
 
-			err = update_nro(p_path.ascii().ptr(), icon.ascii().ptr(), title.ascii().ptr(), author.ascii().ptr(), version.ascii().ptr());
+			String nacp_path = p_path.get_basename() + ".nacp";
+			err = create_nacp(nacp_path, title, author, version);
 			if(err != OK) {
 				return ERR_BUG;
 			}
-
+				
 			String pck_path = p_path.get_basename() + ".pck";
-
-			Vector<SharedObject> so_files;
-
-			err = save_pack(p_preset, pck_path, &so_files);
-
-			if (err == OK && !so_files.empty()) {
-				//if shared object files, copy them
-				da = DirAccess::create(DirAccess::ACCESS_FILESYSTEM);
-				for (int i = 0; i < so_files.size() && err == OK; i++) {
-					err = da->copy(so_files[i].path, p_path.get_base_dir().plus_file(so_files[i].path.get_file()));
-				}
-			}
+			err = save_pack(p_preset, pck_path);
 		}
 
 		memdelete(da);
 		return err;
 	}
 
-	Error update_nro(const char *nro_path, const char *icon_path, const char *title, const char *author, const char *version) {
+	Error create_nacp(String &nacp_path, String& title, String& author, String& version) {
+		const char *title_cstr = title.utf8().ptr();
+		const char *author_cstr = author.utf8().ptr();
+		const char *version_cstr = version.utf8().ptr();
 
-		NroHeader *nro_hdr;
-		AssetHeader *asset_hdr;
-		NacpStruct *nacp;
+		NacpStruct *nacp = (NacpStruct *)malloc(sizeof(NacpStruct));
+		memset(nacp, 0, sizeof(NacpStruct));
 
-		printf("\nupdate_nro: %s, %s, %s, %s, %s\n", nro_path, icon_path, title, author, version);
-
-		// read nro header to memory
-		printf("NRO: reading nro header, %li bytes @ %li\n", sizeof(NroHeader), sizeof(NroStart));
-		nro_hdr = reinterpret_cast<NroHeader *>(read_bytes(nro_path, sizeof(NroStart), sizeof(NroHeader)));
-		if (nro_hdr == nullptr) {
-			printf("error: could not read nro header\n");
-			return ERR_INVALID_DATA;
+		for (int i = 0; i < 12; i++) {
+			strncpy(nacp->lang[i].name, title_cstr, sizeof(nacp->lang[i].name) - 1);
+			strncpy(nacp->lang[i].author, author_cstr, sizeof(nacp->lang[i].author) - 1);
 		}
-		// is nro header valid?
-		printf("NRO: checking nro header magic\n");
-		if (strncmp((char *) nro_hdr->Magic, "NRO0", 4) != 0) {
-			printf("error: nro magic not found\n");
-			free(nro_hdr);
-			return ERR_INVALID_DATA;
-		}
-		// nro header read success
-		printf("NRO: magic: %s, asset header offset: %i\n", nro_hdr->Magic, nro_hdr->size);
-
-		// read asset header to memory
-		printf("ASSET: reading asset header, %li bytes @ %i\n", sizeof(AssetHeader), nro_hdr->size);
-		asset_hdr = reinterpret_cast<AssetHeader *>(read_bytes(nro_path, nro_hdr->size, sizeof(AssetHeader)));
-		if (asset_hdr == nullptr) {
-			printf("error: could not read asset header\n");
-			free(nro_hdr);
-			return ERR_INVALID_DATA;
-		}
-		// is asset header valid?
-		printf("ASSET: checking asset header magic\n");
-		if (strncmp((char *) asset_hdr->magic, "ASET", 4) != 0) {
-			printf("error: asset magic not found\n");
-			free(asset_hdr);
-			free(nro_hdr);
-			return ERR_INVALID_DATA;
-		}
-		// asset header read success
-		printf("ASSET: magic: %s, version: %i, icon offset: %li, icon size: %li\n",
-			   asset_hdr->magic, asset_hdr->version, asset_hdr->icon.offset, asset_hdr->icon.size);
-
-		// update icon
-		int icon = 1;
-		size_t wrote = 0;
-		size_t icon_size = 0;
-		size_t icon_offset = nro_hdr->size + asset_hdr->icon.offset;
-		printf("ICON: reading new icon from %s\n", icon_path);
-		unsigned char *icon_data = read_file(icon_path, &icon_size);
-		if (icon_data == nullptr) {
-			printf("ICON: no icon set in editor, using default icon\n");
-			icon_data = (unsigned char *) icon_default;
-			icon_size = icon_default_size;
-			icon = 0;
-		}
-
-		// don't overwrite icon if size > dummy icon size
-		if(icon_size > asset_hdr->icon.size) {
-			printf("error: new icon size (%li) > %li\n", icon_size, asset_hdr->icon.size);
-			if(icon) {
-				free(icon_data);
-			}
-			free(asset_hdr);
-			free(nro_hdr);
-			return ERR_INVALID_DATA;
-		}
-
-		printf("ICON: writing new icon, %li bytes @ %li\n", icon_size, icon_offset);
-		wrote = write_bytes(nro_path, icon_offset, icon_size, icon_data);
-		if (wrote != icon_size) {
-			printf("error: could not write icon data\n");
-			if(icon) {
-				free(icon_data);
-			}
-			free(asset_hdr);
-			free(nro_hdr);
-			return ERR_INVALID_DATA;
-		}
-		if(icon) {
-			free(icon_data);
-		}
-
-		// update asset header
-		printf("ASSET: writing new asset header, %li bytes @ %i\n", sizeof(AssetHeader), nro_hdr->size);
-		asset_hdr->icon.size = icon_size;
-		wrote = write_bytes(nro_path, nro_hdr->size, sizeof(AssetHeader), (const unsigned char *) asset_hdr);
-		if (wrote != sizeof(AssetHeader)) {
-			printf("error: could not write asset header\n");
-			free(asset_hdr);
-			free(nro_hdr);
-			return ERR_INVALID_DATA;
-		}
-
-		// read nacp to memory
-		if(asset_hdr->nacp.size > 0) {
-			// asset_hdr->nacp.size should always be > 0 as we pack it with elf2nro @SCsub
-			size_t nacp_offset = nro_hdr->size + asset_hdr->nacp.offset;
-			printf("NACP: reading nacp, %li bytes @ %li\n", asset_hdr->nacp.size, nacp_offset);
-			nacp = reinterpret_cast<NacpStruct *>(read_bytes(nro_path, nacp_offset, asset_hdr->nacp.size));
-			if (nacp == nullptr) {
-				printf("error: could not read nacp\n");
-				free(asset_hdr);
-				free(nro_hdr);
-				return ERR_INVALID_DATA;
-			}
-			// nacp read success
-			// update nacp title and author
-			for (int i = 0; i < 12; i++) {
-				strncpy(nacp->lang[i].name, title, sizeof(nacp->lang[i].name) - 1);
-				strncpy(nacp->lang[i].author, author, sizeof(nacp->lang[i].author) - 1);
-			}
-			strncpy(nacp->version, version, sizeof(nacp->version) - 1);
-			// write nacp back
-			printf("NACP: writing new nacp, %li bytes @ %li\n", asset_hdr->nacp.size, nacp_offset);
-			wrote = write_bytes(nro_path, nacp_offset, asset_hdr->nacp.size, (const unsigned char *) nacp);
-			if (wrote != asset_hdr->nacp.size) {
-				printf("error: could not write nacp\n");
-				free(nacp);
-				free(asset_hdr);
-				free(nro_hdr);
-				return ERR_INVALID_DATA;
-			}
-			free(nacp);
-		}
-
-		printf("update_nro: done\n");
-		free(asset_hdr);
-		free(nro_hdr);
+		strncpy(nacp->version, version_cstr, sizeof(nacp->version) - 1);
+		
+		FileAccess *nacp_file = FileAccess::open(nacp_path, FileAccess::WRITE);
+		nacp_file->store_buffer((const uint8_t*)nacp, sizeof(NacpStruct));
+		nacp_file->close();
+		free(nacp);
 
 		return OK;
 	}
@@ -482,107 +361,6 @@ public:
 	}
 };
 
-///
-/// SWITCH NRO UTILS
-///
-unsigned char *read_file(const char *fn, size_t *len_out) {
-
-	FILE *fd = fopen(fn, "rb");
-	if (fd == nullptr) {
-		printf("read_file: error: fopen failed\n");
-		return nullptr;
-	}
-
-	fseek(fd, 0, SEEK_END);
-	size_t len = ftell(fd);
-	fseek(fd, 0, SEEK_SET);
-
-	auto buf = (unsigned char *) malloc(len);
-	if (buf == nullptr) {
-		printf("read_file: error: malloc failed\n");
-		fclose(fd);
-		return nullptr;
-	}
-
-	size_t rc = fread(buf, 1, len, fd);
-	if (rc != len) {
-		fclose(fd);
-		free(buf);
-		printf("read_file: error: fread failed\n");
-		return nullptr;
-	}
-
-	fclose(fd);
-
-	*len_out = len;
-	return buf;
-}
-
-unsigned char *read_bytes(const char *fn, size_t off, size_t len) {
-
-	FILE *fd = fopen(fn, "rb");
-	if (fd == nullptr) {
-		printf("read_bytes: error: fopen failed\n");
-		return nullptr;
-	}
-
-	fseek(fd, 0, SEEK_END);
-	size_t size = ftell(fd);
-	if (off + len > size) {
-		printf("read_bytes: error: offset + len > size\n");
-		fclose(fd);
-		return nullptr;
-	}
-
-	auto buf = (unsigned char *) malloc(len);
-	if (buf == nullptr) {
-		printf("read_file: error: malloc failed\n");
-		fclose(fd);
-		return nullptr;
-	}
-
-	fseek(fd, off, SEEK_SET);
-	size_t rc = fread(buf, 1, len, fd);
-	if (rc != len) {
-		fclose(fd);
-		free(buf);
-		printf("read_file: error: fread failed\n");
-		return nullptr;
-	}
-
-	fclose(fd);
-
-	return buf;
-}
-
-size_t write_bytes(const char *fn, size_t off, size_t len, const unsigned char *data) {
-
-	FILE *fd = fopen(fn, "rb+");
-	if (fd == nullptr) {
-		printf("write_bytes: error: fopen failed\n");
-		return 0;
-	}
-
-	fseek(fd, 0, SEEK_END);
-	size_t size = ftell(fd);
-	if (off + len > size) {
-		printf("write_bytes: error: offset + len > size\n");
-		fclose(fd);
-		return 0;
-	}
-
-	fseek(fd, off, SEEK_SET);
-	size_t rc = fwrite(data, 1, len, fd);
-	if (rc != len) {
-		fclose(fd);
-		printf("write_bytes: error: fwrite failed, written = %li\n", rc);
-		return 0;
-	}
-
-	fclose(fd);
-
-	return rc;
-}
 
 void register_switch_exporter() {
 	String exe_ext;
