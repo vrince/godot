4c351e5c306a624d1f45bc8b64e66ffae0f9e381
Ezekiel Bethel <zek@9net.org>
2020-06-29 15:41:24 +0100

    hopefully fix godot nro filename

--- scripts/create-switch-release.sh
+++ scripts/create-switch-release.sh
@@ -7,8 +7,10 @@ fi
 sudo apt-get update
 sudo apt-get -y install zip
 
+version_number=`echo $1 | grep -o '[0-9]\+.[0-9]\+.[0-9]\+'`
+
 mkdir -p switch/godot config/nx-hbmenu/fileassoc
 cp godot-forwarder.nro switch/godot/godot-forwarder.nro
-cp switch_release_debug.nro switch/godot/godot-$1.nro
+cp switch_release_debug.nro switch/godot/godot-${version_number}.nro
 cp platform/switch/godot.cfg config/nx-hbmenu/fileassoc/godot.cfg
 zip -r godot-switch.zip switch config
