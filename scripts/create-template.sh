e9789415b79f1424be7c3d1a8a5f5c605a7a3495
Ezekiel Bethel <zek@9net.org>
2020-08-14 00:06:48 +0100

    dont syu, add version.txt to template zip

--- scripts/create-template.sh
+++ scripts/create-template.sh
@@ -3,7 +3,10 @@
 sudo apt-get update
 sudo apt-get -y install zip
 
+template_version=`echo $1 | grep -Po '[0-9]+.[0-9]+.[0-9]+(-[^-]*)?' | sed s/-/./`
+
 mkdir templates
 cp switch_release_debug.nro templates/switch_release.nro
 cp platform/switch/romfs/applet_splash.rgba.gz templates/switch_applet_splash.rgba.gz
+echo $template_version > templates/version.txt
 zip -r godot_templates_switch_only.tpz templates
